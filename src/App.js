a79fbda4668a9e96f4f306de6ea5cc804bae5ca4
Eduardo <emendoza3843@ppsc.edu.mx>
2021-03-22 17:08:29 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import imagenVenus from "./assets/images/venus.jpg";
+import imagenAdan from "./assets/images/creacion-adan.jpg";
+import imagenDali from "./assets/images/persistencia-memoria.png";
 
 function App() {
+  const data = [
+    {
+      title: "El nacimiento de Venus",
+      description: "Cuadro de La bodega Marina",
+      category: "Histórico",
+      image: imagenVenus,
+      autor: "Sandro Botticelli",
+    },
+    {
+      title: "La creación de Adán",
+      description: "Parte de la capilla sixtina",
+      category: "Arte religioso",
+      image: imagenAdan,
+      autor: "Miguel Ángel",
+    },
+    {
+      title: "La persistencia de la memoria",
+      description: "Obra de Salvador Dalí",
+      category: "Moderno",
+      image: imagenDali,
+      autor: "Salvador Dalí",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {data.map((item, i) => {
+          return <ArtCard {...item} key={item.title + i} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function ArtCard(props) {
+  return (
+    <div className="card">
+      <img className="card_image" src={props.image} alt="" />
+      <div className="card_info">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <h6>{props.category}</h6>
+        <small>Autor: {props.autor}</small>
+      </div>
     </div>
   );
 }
