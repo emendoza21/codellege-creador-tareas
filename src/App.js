60a25959e785353f4491a6733c06b9009ba2304e
Eduardo <emendoza3843@ppsc.edu.mx>
2021-03-23 18:31:44 -0600

    agregar form email y form obras

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React from "react";
 import "./App.css";
-import imagenVenus from "./assets/images/venus.jpg";
-import imagenAdan from "./assets/images/creacion-adan.jpg";
-import imagenDali from "./assets/images/persistencia-memoria.png";
+import imagenEstrellada from "./assets/images/noche_estrellada.jpg";
+import imagenOla from "./assets/images/ola.jpg";
+import imagenGrito from "./assets/images/grito.jpg";
+import imagenJuicio from "./assets/images/juicio.jpg";
+import imagenAvigno from "./assets/images/avigno.jpg";
+import imagenLisa from "./assets/images/lisaa.jpg";
 
 function App() {
   const data = [
     {
-      title: "El nacimiento de Venus",
-      description: "Cuadro de La bodega Marina",
+      title: "La noche estrellada",
+      description: "Una de las obras más famosas",
       category: "Histórico",
-      image: imagenVenus,
-      autor: "Sandro Botticelli",
+      image: imagenEstrellada,
+      autor: "VINCENT VAN GOGH",
     },
     {
-      title: "La creación de Adán",
-      description: "Parte de la capilla sixtina",
-      category: "Arte religioso",
-      image: imagenAdan,
-      autor: "Miguel Ángel",
+      title: "La Ola",
+      description: "figuras de la escuela Ukiyo-e",
+      category: "Arte Abstracto",
+      image: imagenOla,
+      autor: "KATSUSHIKA HOKUSAI",
     },
     {
-      title: "La persistencia de la memoria",
-      description: "Obra de Salvador Dalí",
+      title: "El Grito",
+      description: "pintura más reconocibles",
       category: "Moderno",
-      image: imagenDali,
-      autor: "Salvador Dalí",
+      image: imagenGrito,
+      autor: "EDVARD MUNCH",
+    },
+    {
+      title: "El Juicio Final",
+      description: "fresco en la Capilla Sixtina",
+      category: "Arte Religioso",
+      image: imagenJuicio,
+      autor: "MIGUEL ANGEL",
+    },
+    {
+      title: "Las Señoritas de Avigno",
+      description: "inició el cubismo",
+      category: "Arte moderno",
+      image: imagenAvigno,
+      autor: "PABLO PICASSO",
+    },
+    {
+      title: "Mona Lisa ",
+      description: "óleo sobre tabla",
+      category: "Historico",
+      image: imagenLisa,
+      autor: "LEONARDO DA VINCI",
     },
   ];
 
   return (
     <div className="App">
       <div className="container">
+        <Form />
+        <Obras />
         {data.map((item, i) => {
           return <ArtCard {...item} key={item.title + i} />;
         })}
@@ -40,6 +66,147 @@ function App() {
   );
 }
 
+// Form email
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      para: "",
+      asunto: "",
+      mensaje: "",
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event);
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("mensaje enviado");
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <form className="form">
+        <div className="mensaje">Mensaje</div>
+        <div>
+          <input
+            name="para"
+            placeholder="Para: "
+            type="email"
+            value={this.state.para}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            name="asunto"
+            placeholder="Asunto: "
+            type="text"
+            value={this.state.asunto}
+            onChange={this.handleChange}
+          />
+        </div>
+        {/* <div>
+          <label for="category">Categoría</label>
+          <select
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          >
+            <option value=""></option>
+            <option value="Histórico">Histórico</option>
+            <option value="Moderno">Moderno</option>
+            <option value="Arte abstracto">Arte abstracto</option>
+            <option value="Religioso">Religioso</option>
+          </select>
+        </div> */}
+        <div>
+          <textarea
+            name="mensaje"
+            value={this.state.mensaje}
+            onChange={this.handleChange}
+          ></textarea>
+        </div>
+        <input
+          className="submit"
+          type="submit"
+          value="Enviar"
+          onClick={this.handleSubmit}
+        />
+      </form>
+    );
+  }
+}
+// Form email
+
+//Form Obras
+class Obras extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      description: "",
+      category: "",
+      image: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form className="form_obras">
+        <div>
+          <label for="title">Título de la obra de arte</label>
+          <input
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label for="description">Descripción</label>
+          <input
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label for="category">Categoría</label>
+          <select
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          >
+            <option value=""></option>
+            <option value="Histórico">Histórico</option>
+            <option value="Moderno">Moderno</option>
+            <option value="Arte abstracto">Arte abstracto</option>
+            <option value="Religioso">Religioso</option>
+          </select>
+        </div>
+        <div>
+          <label for="image">URL de la imagen</label>
+          <input
+            name="image"
+            value={this.state.image}
+            onChange={this.handleChange}
+          />
+        </div>
+        <input className="submit" type="submit" value="Guardar obra de arte" />
+      </form>
+    );
+  }
+}
+
+// Tarjeta
 function ArtCard(props) {
   return (
     <div className="card">
@@ -48,10 +215,34 @@ function ArtCard(props) {
         <h1>{props.title}</h1>
         <p>{props.description}</p>
         <h6>{props.category}</h6>
-        <small>Autor: {props.autor}</small>
+        <h5>De: {props.autor}</h5>
       </div>
+      <Button />
     </div>
   );
 }
+// Tarjeta
+// Boton
+class Button extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicks: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  };
+
+  render() {
+    return (
+      <button className="likes_button" onClick={this.handleClick}>
+        {this.state.clicks} likes
+      </button>
+    );
+  }
+}
+// Boton
 
 export default App;
